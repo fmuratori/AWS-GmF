66c9786c1d5b1ec7f8eee48fa3f23ba7c685c1e0
Luca Bracchi <luca.bracchi3@studio.unibo.it>
2021-10-24 11:22:43 +0200

    added mocha & chai test support

---
+++ backend/test/test.js
@@ -0,0 +1,9 @@
+const { expect } = require("chai")
+
+// const donationController = new DonationController()
+
+describe("easy test", () => {
+    it("shoud pass", () => {
+        expect(1).to.be.a("number")
+    })
+})
