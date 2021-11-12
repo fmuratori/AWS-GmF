564a43f8860828c89963310f2f038cc62017f82a
Luca Bracchi <luca.bracchi3@studio.unibo.it>
2021-11-12 11:00:54 +0100

    fixed api url creation

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,5 +1,4 @@
 import axios from "axios";
-import store from "./store";
 
 import { EventPayload, PackPayload, FoodPayload } from "./types";
 
@@ -9,34 +8,34 @@ export default {
   /* events */
 
   async createEvent(payload: EventPayload) {
-    return axios.post(`${process.env.API_URL}/api/event/add`, payload);
+    return axios.post(`${process.env.VUE_APP_API_URL}/api/event/add`, payload);
   },
 
   async editEvent(payload: EventPayload) {
-    return axios.post(`${process.env.API_URL}/api/event/edit`, payload);
+    return axios.post(`${process.env.VUE_APP_API_URL}/api/event/edit`, payload);
   },
 
   async eventList(payload: any) {
-    return axios.post(`${process.env.API_URL}/api/event/find`, payload);
+    return axios.post(`${process.env.VUE_APP_API_URL}/api/event/find`, payload);
   },
 
   /* packs */
 
   async createPack(payload: PackPayload) {
-    return axios.post(`${process.env.API_URL}/api/pack/add`, payload);
+    return axios.post(`${process.env.VUE_APP_API_URL}/api/pack/add`, payload);
   },
 
   async packList(payload: any) {
-    return axios.post(`${process.env.API_URL}/api/pack/find`, payload);
+    return axios.post(`${process.env.VUE_APP_API_URL}/api/pack/find`, payload);
   },
 
   /* foods */
 
   async addFood(payload: FoodPayload) {
-    return axios.post(`${process.env.API_URL}/api/food/add-or-update`, payload);
+    return axios.post(`${process.env.VUE_APP_API_URL}/api/food/add-or-update`, payload);
   },
 
   async foodList(payload: any) {
-    return axios.post(`${process.env.API_URL}/api/food/find`, payload);
+    return axios.post(`${process.env.VUE_APP_API_URL}/api/food/find`, payload);
   },
 };
