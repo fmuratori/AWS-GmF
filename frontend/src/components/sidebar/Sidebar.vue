268d45480c80e2b4c3989f12de4e4f540c187575
Luca Bracchi <luca.bracchi3@studio.unibo.it>
2021-11-12 10:59:55 +0100

    refactored sidebar

--- frontend/src/components/sidebar/Sidebar.vue
+++ frontend/src/components/sidebar/Sidebar.vue
@@ -1,140 +1,66 @@
 <template lang="pug">
 #sidebar.p-3(v-if="isSidebarOpen()")
   #sidebar-user 
-    b-row.p-4(no-gutters align-h="center") 
-      b-col(class="mr-1" cols="auto")
+    b-row.p-4(no-gutters, align-h="center") 
+      b-col.mr-1(cols="auto")
         b-icon-person-circle(font-scale="3")
       b-col.ml-1(cols="auto")
         p {{ userFullname }}
         p {{ this.$store.state.session.userData.type }}
-        p(@click="changePage('ManagerEditUserInfo')" class="clickable") edit
+        p.clickable(@click="changePage('ManagerEditUserInfo')") edit
 
-    hr(class="sidebar-hr my-3")
+    hr.sidebar-hr.my-3
 
-  div(id="sidebar-actions") 
-  
+  #sidebar-actions 
     div(v-if="$store.state.session.userData.type == 'user'")
-      b-row(class="pb-2" no-gutters align-v="center")
-        b-col(class="mr-1" cols="auto")
-          font-awesome-icon(icon="utensils" size="lg")
-        b-col(class="ml-1")
-          h6 Dona cibo
+      SidebarCategory(text="Donations", icon="map")
+      SidebarItem(text="Make a donation", route="ManagerDonationsCreate")
+      SidebarItem(text="Donation list", route="ManagerDonationsUserList")
 
-      b-row(class="pl-3 pr-1 sidebar-item" no-gutters align-v="center" @click="changePage('ManagerDonationsCreate')" 
-      :class="{ 'sidebar-item-selected': isRouteSelected('ManagerDonationsCreate') }")
-        b-col
-          label(class="py-1") Crea una donazione
-        b-col(cols="auto")
-          b-icon(icon="chevron-right")
-
-      b-row(class="pl-3 pr-1 sidebar-item" no-gutters align-v="center" @click="changePage('ManagerDonationsUserList')" 
-      :class="{ 'sidebar-item-selected': isRouteSelected('ManagerDonationsUserList') }")
-        b-col
-          label(class="py-1") Tue donazioni
-        b-col(cols="auto")
-          b-icon(icon="chevron-right")
-
-      hr(class="sidebar-hr my-3")
+      hr.sidebar-hr.my-3
 
     div(v-if="this.$store.state.session.userData.type == 'volunteer'")
-      b-row(class="pb-2" no-gutters align-v="center")
-        b-col(class="mr-1" cols="auto")
-          b-icon(icon="map" size="lg")
-        b-col(class="ml-1")
-          h6 Donazioni
+      SidebarCategory(text="Donations", icon="map")
+      SidebarItem(
+        text="Create retrieve assignment",
+        route="ManagerDonationsRetrieve"
+      )
+      SidebarItem(text="Donation list", route="ManagerDonationsVolunteerList")
 
-      b-row(class="pl-3 pr-1 sidebar-item" no-gutters align-v="center" @click="changePage('ManagerDonationsRetrieve')" 
-      :class="{ 'sidebar-item-selected': isRouteSelected('ManagerDonationsRetrieve') }")
-        b-col
-          label(class="py-1") Crea incarico
-        b-col(cols="auto")
-          b-icon(icon="chevron-right")
+      hr.sidebar-hr.my-3
 
-      b-row(class="pl-3 pr-1 sidebar-item" no-gutters align-v="center" @click="changePage('ManagerDonationsVolunteerList')" 
-      :class="{ 'sidebar-item-selected': isRouteSelected('ManagerDonationsVolunteerList') }")
-        b-col
-          label(class="py-1") Tuoi incarichi
-        b-col(cols="auto")
-          b-icon(icon="chevron-right")
-      
-      hr(class="sidebar-hr my-3")
+    div(
+      v-if="['user', 'volunteer'].includes($store.state.session.userData.type)"
+    )
+      SidebarCategory(text="Families", icon="users")
+      SidebarItem(text="Report a family", route="ManagerFamilySubscribe")
+      SidebarItem(text="Report list", route="ManagerFamilyList")
 
-
-    div(v-if="['user', 'volunteer'].includes($store.state.session.userData.type)")
-      b-row(class="pb-2" no-gutters align-v="center")
-        b-col(class="mr-1" cols="auto")
-          font-awesome-icon(icon="users" size="lg")
-        b-col(class="ml-1")
-          h6 Famiglie bisognose
-
-      b-row(class="pl-3 pr-1 sidebar-item" no-gutters align-v="center" @click="changePage('ManagerFamiliesSubscribe')" 
-      :class="{ 'sidebar-item-selected': isRouteSelected('ManagerFamiliesSubscribe') }")
-        b-col
-          label(class="py-1") Segnala famiglia
-        b-col(cols="auto")
-          b-icon(icon="chevron-right")
-
-      b-row(class="pl-3 pr-1 sidebar-item" no-gutters align-v="center" @click="changePage('ManagerFamiliesList')" 
-      :class="{ 'sidebar-item-selected': isRouteSelected('ManagerFamiliesList') }")
-        b-col
-          label(class="py-1") Tue segnalazioni
-        b-col(cols="auto")
-          b-icon(icon="chevron-right")
-
-      hr(class="sidebar-hr my-3")
+      hr.sidebar-hr.my-3
 
     div(v-if="this.$store.state.session.userData.type == 'volunteer'")
-      b-row(class="pb-2" no-gutters align-v="center")
-        b-col(class="mr-1" cols="auto")
-          b-icon(icon="calendar-event" size="lg")
-        b-col(class="ml-1")
-          h6 Eventi e raduni
+      SidebarCategory(text="Events", icon="calendar")
+      SidebarItem(text="Create an event", route="ManagerEventCreate")
+      SidebarItem(text="Your events", route="ManagerEvents")
 
-      b-row(class="pl-3 pr-1 sidebar-item" no-gutters align-v="center" @click="changePage('ManagerEventCreate')" 
-      :class="{ 'sidebar-item-selected': isRouteSelected('ManagerEventCreate') }")
-        b-col
-          label(class="py-1") Crea un evento
-        b-col(cols="auto")
-          b-icon(icon="chevron-right")
-
-      b-row(class="pl-3 pr-1 sidebar-item" no-gutters align-v="center" @click="changePage('ManagerEvents')" 
-      :class="{ 'sidebar-item-selected': isRouteSelected('ManagerEvents') }")
-        b-col
-          label(class="py-1") Gestisci eventi
-        b-col(cols="auto")
-          b-icon(icon="chevron-right")
-      hr(class="sidebar-hr my-3")
+      hr.sidebar-hr.my-3
 
     div(v-if="this.$store.state.session.userData.type != 'user'")
-      b-row(class="pl-3 pr-1 sidebar-item" no-gutters align-v="center" @click="changePage('ManagerFood')" 
-      :class="{ 'sidebar-item-selected': isRouteSelected('ManagerFood') }")
-        b-col
-          label(class="py-1") Food manager
-        b-col(cols="auto")
-          b-icon(icon="chevron-right")
-
-    hr(class="sidebar-hr my-3")
-
-    div(v-if="this.$store.state.session.userData.type != 'user'")
-      b-row(class="pl-3 pr-1 sidebar-item" no-gutters align-v="center" @click="changePage('ManagerFood')" 
-      :class="{ 'sidebar-item-selected': isRouteSelected('ManagerFood') }")
-        b-col
-          label(class="py-1") Food manager
-        b-col(cols="auto")
-          b-icon(icon="chevron-right")
-
-    div(v-if="this.$store.state.session.userData.type != 'user'")
-      //- SidebarItem(text= 'Food manager', route='ManagerFood')
+      SidebarItem(text= 'Food manager', route='ManagerFood')
       //- SidebarItem(text= 'Pack manager', route='ManagerPacks')
-
 </template>
 
 <script lang="ts">
 import Vue from "vue";
+import SidebarCategory from "./SidebarCategory.vue";
+import SidebarItem from "./SidebarItem.vue";
 
 export default Vue.extend({
   name: "Sidebar",
-  components: {},
+  components: {
+    SidebarCategory,
+    SidebarItem,
+  },
   data: function () {
     return {};
   },
@@ -151,18 +77,6 @@ export default Vue.extend({
         return fullname.substring(0, 16) + "...";
       }
       return fullname;
-    },
-    userType() {
-      switch (this.$store.state.session.userData.type) {
-        case "user":
-          return "Utente";
-        case "volunteer":
-          return "Volontario";
-        case "trusted":
-          return "Collaboratore";
-        default:
-          return "UNKNOWN";
-      }
     },
   },
   methods: {
--- frontend/src/components/sidebar/SidebarCategory.vue
+++ frontend/src/components/sidebar/SidebarCategory.vue
@@ -3,6 +3,7 @@ b-row.pb-2(no-gutters align-v="center")
   b-col.mr-1(cols="auto")
     font-awesome-icon(v-if="this.icon == 'users'" icon="users" size="lg")
     font-awesome-icon(v-else-if="this.icon == 'calendar'" icon="calendar" size="lg")
+    font-awesome-icon(v-else-if="this.icon == 'map'" icon="map" size="lg")
     font-awesome-icon(v-else-if="this.icon == 'drumstick-bite'" icon="drumstick-bite" size="lg")
   b-col.ml-1
     h6 {{text}}
